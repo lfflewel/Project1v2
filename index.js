4f1e157363ab49813c1b51b37f1188f6426b0192
CLe440 <89447647+CLe440@users.noreply.github.com>
2021-09-21 23:28:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
-// random
-const loginCover = document.getElementById("loginCover");
-const signupCover = document.getElementById("signupCover");
-const loginForm = document.getElementById("loginForm");
-const signupForm = document.getElementById("signupForm");
 
-// hide the login/sign up form 
-
-function toggleLoginSignup(location) {
-    if (location === "login") {
-        loginCover.classList.add("hide");
-        loginForm.classList.remove("hide");
-        signupForm.classList.add("hide");
-        signupCover.classList.remove("hide");
-    }
-    else if (location == "signup") {
-        signupCover.classList.add("hide");
-        signupForm.classList.remove("hide");
-        loginForm.classList.add("hide");
-        loginCover.classList.remove("hide")
-    }
-}
-
-// -----------
 // constants to run project with Node.js and express Js
 const express = require('express');
 const app = express();
